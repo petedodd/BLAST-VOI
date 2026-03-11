0187670c628f9f863d58059a025b232b32f2ab3b
PJ Dodd <p.j.dodd@sheffield.ac.uk>
2026-03-11 08:44:12 +0000

    cleaning up

--- R/0_flux_approx.R
+++ R/0_flux_approx.R
@@ -1,8 +1,13 @@
 ## exploring and justifying flux approximations
+library(here)
+library(data.table)
 library(expm)
 library(ggplot2)
-library(data.table)
-library(here)
+library(ggrepel)
+library(readxl)
+library(sf)
+library(BLASTtbmod)
+
 
 ## ========= PART 1: analytical approximations to dynamics
 
@@ -188,19 +193,9 @@ gp
 ggsave(gp, filename = here("output/x_fluxapp.png"), w = 7, h = 5)
 
 
-## ========= PART 2:
+## ========= PART 2: fluxes as inputs
 
-
-## TODO simulation of this and comparison with mixing
 rm(list = ls())
-
-library(here)
-library(BLASTtbmod)
-library(data.table)
-library(readxl)
-library(ggplot2)
-library(ggrepel)
-library(sf)
 
 ## === trying different data for
 
@@ -215,31 +210,6 @@ abline(a = 0, b = 1, col = 2)
 FM <- read_excel(here("data/PhyloFlows_7Zone.xlsx"))
 FM <- FM[, 2:ncol(FM)]
 FM <- as.matrix(FM)
-
-## === running the model
-
-## define other parameters
-n_particles <- 10
-time <- 1 # NOTE not used TODO check
-start_year <- 2015
-years <- 6
-
-## NOTE on prevalence for initial state
-## prev is split across SC and D in 1:1 ratio - so need to double expectation (see init_D/SC)
-## is only applied to (1-exp(-ari0*ageMids)) of pop, so need to divide by this
-## PPP <- fread(here("data/PPP.csv"))
-## PPP$ab <- PPP$posterior.a + PPP$posterior.b
-## PPP$prev.mid <- PPP$posterior.a / PPP$ab
-## PPP$patch <- paste("Patch", PPP$zone)
-ageMids <- c(7.5, 32.5, 60) #get from get.parms CHECK used?
-ariparm <- 5e-2 # high?
-
-## plot(prev)
-
-
-## ## TESTING
-## prev <- rep(mean(prev),7) #uniform
-## prev[2:7] <- 0
 
 ## initial prevalences
 dinit <- cbind(rep(0, 7), prev, prev) * 1e-5
@@ -256,34 +226,32 @@ for (i in 1:nrow(MM)) { #loops for transparency
 MM <- MM / max(Re(eigen(MM)$values)) #renormalize
 
 ## create arguments
-args <- get.parms(start_year = start_year, years = years, Dinit = dinit, ari0 = ariparm/10)
+n_particles <- 100
+start_year <- 2015
+years <- 6
+end <- years * 12
+args <- get.parms(start_year = start_year, years = years, Dinit = dinit, ari0 = 5e-3)
 args$beta <- 20
 args$cdr <- 0.7
 args$MM <- MM
 args$popinit <- pops #CHECK correct order in data/function
 
-
- ## ## ## mixing matrix from FM
-## ## mean(FM) / mean(args$MM)
-## ## max(Re(eigen(FM)$values)) / max(Re(eigen(args$MM)$values))
-## ## args$MM <- (max(Re(eigen(args$MM)$values)) / max(Re(eigen(FM)$values))) * t(FM) ## use rescaled flux
-## ## (transpose because rows are from, cols are to)
-
-
-## === new comparisons ===
-end <- 60
-test <- run.model(
+## run model
+output <- run.model(
   args,
   0:end,
-  n.particles = 5,
+  n.particles = n_particles,
   convert = FALSE
 )
 
 
-## check infection flux:
+## get notification flux
 note_flux_idx <- grep("cum_note_flux", BLASTtbmod::get_cols)
 (nmz <- grep("cum_note_flux", BLASTtbmod::get_cols, value = TRUE)) # check
-tmp <- t(test[note_flux_idx, 1, ]) # matrix over time for particle 1 TODO
+tmp <- t(apply(
+  output[note_flux_idx, , ],
+  MARGIN = c(1, 3), FUN = mean
+)) # matrix over time, mean over particles
 matplot(tmp, type = "l")           #plot
 note_flux <- matrix(
   tail(tmp, n = 1), # last for cumulative
@@ -292,11 +260,14 @@ note_flux <- matrix(
 )
 note_flux <- note_flux / max(note_flux) #normalize
 
+## get infection flux
 inf_flux_idx <- grep("cum_inf_flux", BLASTtbmod::get_cols)
 (nmz <- grep("cum_inf_flux", BLASTtbmod::get_cols, value = TRUE)) # check
-tmp <- t(test[inf_flux_idx, 1, ]) # matrix over time for particle 1 TODO
+tmp <- t(apply(
+  output[inf_flux_idx, , ],
+  MARGIN = c(1, 3), FUN = mean
+)) # matrix over time, mean over particles
 matplot(tmp, type = "l")          #plot
-## empirical infection flux
 inf_flux <- matrix(
   tail(tmp, n = 1),
   ncol = args$patch_dims, nrow = args$patch_dims
@@ -311,42 +282,6 @@ CF <- data.table(
 )
 CF[, variable := gsub("cum_inf_flux", "", nmz)]
 CF[, fromid := gsub("\\[", "", gsub(",.\\]", "", variable))]
-
-
-## data vs FOI
-ggplot(
-  CF,
-  aes(
-    x = genomic.flux, y = output.FOI.flux,
-    label = variable, shape = fromid, col = fromid
-  )
-) +
-  geom_abline(intercept = 0, slope = 1, col = 2) +
-  scale_shape_manual(values = 0:6) +
-  geom_point(size = 2) +
-  geom_text_repel(show.legend = FALSE) +
-  theme_classic() +
-  ggpubr::grids()
-
-## ggsave(here("plots/flux_cfFOI.png"), w = 7, h = 6)
-
-
-## data vs note
-ggplot(
-  CF,
-  aes(
-    x = genomic.flux, y = output.note.flux,
-    label = variable, shape = fromid, col = fromid
-  )
-) +
-  geom_abline(intercept = 0, slope = 1, col = 2) +
-  scale_shape_manual(values = 0:6) +
-  geom_point(size = 2) +
-  geom_text_repel(show.legend = FALSE) +
-  theme_classic() +
-  ggpubr::grids()
-
-## ggsave(here("plots/flux_cfnote.png"), w = 7, h = 6)
 
 ## FOI vs note
 ggplot(
@@ -363,292 +298,33 @@ ggplot(
   theme_classic() +
   ggpubr::grids()
 
-## ggsave(here("plots/flux_notesVfoi.png"), w = 7, h = 6)
-1
+## data vs FOI
+ggplot(
+  CF,
+  aes(
+    x = genomic.flux, y = output.FOI.flux,
+    label = variable, shape = fromid, col = fromid
+  )
+) +
+  geom_abline(intercept = 0, slope = 1, col = 2) +
+  scale_shape_manual(values = 0:6) +
+  geom_point(size = 2) +
+  geom_text_repel(show.legend = FALSE) +
+  theme_classic() +
+  ggpubr::grids()
 
 
-## (nmz <- grep("Tijk", BLASTtbmod::get_cols, value = TRUE)) # check
-## (nmz <- grep("Sij", BLASTtbmod::get_cols, value = TRUE)) # check
-## (nmz <- grep("cum_note_flux", BLASTtbmod::get_cols, value = TRUE)) # check
-## (nmz <- grep("Sij", BLASTtbmod::get_cols, value = TRUE)) # check
-## idx <- grep("Sij", BLASTtbmod::get_cols)
-## tmp <- t(test[idx, 1, ]) # matrix over time for particle 1
-## matplot(tmp, type = "l")
-## str(tmp)
-
-
-## (nmz <- grep("Tijk", BLASTtbmod::get_cols, value = TRUE)) # check
-## idx <- grep("Tijk", BLASTtbmod::get_cols)
-## tmp <- t(test[idx, 1, ]) # matrix over time for particle 1
-## matplot(tmp, type = "l")
-## str(tmp)
-
-## TIJK <- array(tmp[end, ], dim = c(7, 7, 6)) # last time
-## TIJ <- matrix(0,7,7)
-## for(k in 1:6) TIJ <- TIJ + TIJK[,,k]
-
-## TIJ
-
-
-## (nmz2 <- grep("cum_inf_flux", BLASTtbmod::get_cols, value = TRUE)) # check
-## idx2 <- grep("cum_inf_flux", BLASTtbmod::get_cols)
-## tmp2 <- t(test[idx2, 1, ]) # matrix over time for particle 1
-## matplot(tmp2, type = "l")
-## str(tmp2)
-
-## FF <- matrix(tmp2[end,],7,7) #last time
-
-## plot(
-##   TIJ / sum(TIJ),
-##   FF / sum(FF)
-## )
-
-## abline(a=0,b=1,col=2)
-
-## ## :-)
-
-## ## NOTE see bottom for parameter computations
-## ## what about ellij?
-## ## OK: calculated with version with cum_note_flux acumulates ellij
-## ## OK: noisier with sampling against foitemp but OK
-## (nmz3 <- grep("cum_note_flux", BLASTtbmod::get_cols, value = TRUE)) # check
-## idx3 <- grep("cum_note_flux", BLASTtbmod::get_cols)
-## tmp3 <- t(test[idx3, 1, ]) # matrix over time for particle 1
-## matplot(tmp3, type = "l")
-
-## GG <- matrix(tmp3[end,],7,7) #last time
-
-## plot(
-##   GG/sum(GG),
-##   FF/sum(FF)
-## )
-## abline(a=0,b=1,col=2)
-
-## ## :-)
-
-## ========= upper checks
-
-
-## ## run dust model not as particle filter
-## test <- run.model(args,
-##   0:12, # CHECK start time, times etc
-##   n.particles = 50,
-##   convert = FALSE
-## )
-
-## ## checking ordering (prevalence)
-## testo <- extract.pops(test, 1:dim(test)[2], out_type = c("notes", "N", "D", "SC"))
-## testo[varname == "SC", varname := "D"] # merge SC and D
-## testo <- testo[age != "0-14",
-##   .(value = sum(value)),
-##   by = .(particle, step, varname, patch)
-## ] # aggr/adult
-## testo <- dcast(testo, particle+step + patch ~ varname, value.var = "value")
-## ## ggplot(testo[], aes(step, 1e5 * D / N,group=particle)) +
-## ##   geom_line() +
-## ##   facet_wrap(~patch) +
-## ##   expand_limits(y = c(0, NA))
-
-## L <- testo[step == 1]
-## L[, prevmod := 1e5 * D / N]
-## L <- merge(L, data.table(prevdat = prev, patch = paste0("Patch ", 1:7)), by = "patch")
-## LM <- L[,.(prevmod=mean(prevmod),prevdat=mean(prevdat)),by=patch]
-## ggplot(L, aes(prevdat, prevmod, group = patch)) +
-##   geom_boxplot() +
-##   geom_point() +
-##   geom_text(data=LM,aes(label=patch),col="magenta")+
-##   geom_abline(intercept = 0, slope = 1, col = 2)
-
-## ms <- max(testo$step)
-## L <- testo[step %in% c(1,ms)]
-## L[, prevmod := 1e5 * D / N]
-## L <- dcast(L, particle + patch ~ step, value.var = "prevmod")
-## names(L)[3:4] <- c("start","end")
-
-## ## ggplot(L, aes(start, end, group = patch,col=patch)) +
-## ##   geom_point() +
-## ##   stat_ellipse()+
-## ##   geom_abline(intercept = 0, slope = 1, col = 2)+
-## ##   expand_limits(x=c(0,NA),y=c(0,NA))
-
-## L <- merge(L,
-##   data.table(prevdat = prev, patch = paste0("Patch ", 1:7)),
-##   by = "patch"
-## )
-
-## ## ggplot(L, aes(prevdat, end, group = patch, col = patch, label = patch)) +
-## ##   geom_point() +
-## ##   geom_text()
-
-## ## check initial states: pops and prev
-## testp <- extract.pops(test, 1:dim(test)[2], out_type = c( "N", "D", "SC"))
-## testp[varname=="SC",varname:="D"] #merge SC and D
-## testp <- testp[step==1, .(value = sum(value)), by = .(particle, age, varname, patch)] # aggr/adult
-## testp <- testp[, .(value = mean(value)), by = .(age, varname, patch)] # aggr/adult
-## testp[varname=='N',sum(value),by=patch]
-
-
-## idxp <- grep("N\\[1", BLASTtbmod::get_cols)               #pop 1
-## nmzp <- grep("N\\[1", BLASTtbmod::get_cols, value = TRUE) # check
-
-## sum(test[idxp,1,1])
-## pops[1]
-
-
-## ## names
-## ## unique(gsub("[[:punct:]]+|[[:digit:]]+", "", BLASTtbmod::get_cols))
-## ## str(test) # 1262, particles, times if convert = FALSE
-## ## dim(test)
-## ## length(BLASTtbmod::get_cols) #need to update
-
-## ## NOTE
-## ## CHECK get_cols made loc see dev which refers to test/clean
-## ## get_cols <- colnames(y_stocmmod1)[2:ncol(y_stocmmod1)]
-
-## ## cuminf patch
-## idx0 <- grep("cum_inf_ByPatch", BLASTtbmod::get_cols)
-## nmz0 <- grep("cum_inf_ByPatch", BLASTtbmod::get_cols, value = TRUE) # check
-## ## foitemp matrix
-## idx1 <- grep("cum_note_flux", BLASTtbmod::get_cols)
-## nmz1 <- grep("cum_note_flux", BLASTtbmod::get_cols, value = TRUE) # check
-## ## prev
-## idx2 <- grep("PrevByPatch", BLASTtbmod::get_cols)
-## nmz2 <- grep("PrevByPatch", BLASTtbmod::get_cols, value = TRUE) # check
-## ## check infection flux:
-## idx <- grep("cum_inf_flux", BLASTtbmod::get_cols)
-## nmz <- grep("cum_inf_flux", BLASTtbmod::get_cols,value=TRUE) #check
-## tmp <- t(test[idx, 1, ]) # matrix over time for particle 1
-
-## ## matplot(tmp, type = "l")
-
-## ## ## CHECK
-## ## p <- 2
-## ## t(test[idx0, p, 12]) #cumulative infections by patch
-## ## rowSums(matrix(test[idx, p, 12], 7, 7)) #cumulative infections by patch summed
-## ## ## test[idx0, p, 12] / sum(test[idx0, p, 12])
-## ## ## pops/sum(pops)
-
-## ## matrix(nmz,7,7)
-## ## matrix(test[idx, p, 2], 7, 7)
-## ## rowSums(matrix(test[idx, p, 2], 7, 7))
-## ## test[idx0, p, 2]
-## ## ## sum(test[idx0, p, 2]) * pops / sum(pops)
-
-## ## ## FOI:
-## ## matrix(test[idx1, p, 12], 7, 7)
-## ## rowSums(matrix(test[idx1, p, 12], 7, 7)) # OK cst
-## ## chk <- matrix(test[idx1, p, 12], 7, 7)[1, ]
-## ## chk / sum(chk) #from
-## ## pops / sum(pops)
-
-## ## plot(chk / sum(chk), pops * prev / sum(pops * prev),pch=paste0(1:7))
-## ## abline(a = 0, b = 1)
-
-## ## plot(chk / sum(chk), prev / sum(prev), pch = paste0(1:7))
-
-## ## (mldp <- test[idx2, p, 2]) #prev
-## ## plot(1e6 * mldp / prev)
-## ## ## dinit
-## ## prev
-
-## ## plot(prev, 1e5 * mldp, pch = paste0(1:7))
-## ## abline(a = 0, b = 1)
-
-## ## TH = test here
-## ## ## for rerunning
-## test <- run.model(args,
-##   0:12,
-##   n.particles = 5,
-##   convert = FALSE
-## )
-
-
-## ## empirical infection flux
-## tmp <- test[idx, 1:dim(test)[2], dim(test)[3]]   #foi flux matrix
-## tmp0 <- test[idx0, 1:dim(test)[2], dim(test)[3]] #foi flux totals (vec)
-## keep <- abs(colSums(tmp)) < 1e10 # NOTE ditch buggy runs
-## tmp <- rowMeans(test[idx, keep, dim(test)[3]]) # mean over particles at last time
-## tmp0 <- rowMeans(test[idx0, keep, dim(test)[3]]) # mean over particles at last time
-## ## matrix(nmz, ncol = args$patch_dims, nrow = args$patch_dims) # CHECK
-## EIF <- matrix(tmp, ncol = args$patch_dims, nrow = args$patch_dims)
-## nEIF <- EIF / max(EIF)
-## ## FM / max(FM)
-## ## sums
-## ## sum(EIF)
-## ## rowSums(EIF)
-## ## tmp0
-## 1e2 * sum(EIF) / sum(pops)
-## inZ <- rowSums(EIF)
-## ## rowSums(FM) * sum(EIF)
-## fromZ <- colSums(EIF)
-## ## colSums(FM) * sum(EIF)
-
-## ## random mix, even prev expt:
-## pops / sum(pops)
-## inZ / sum(inZ) #flux into == tmp0/sum(tmp0)
-## fromZ / sum(fromZ)
-
-## ## random-mixing expectation: proportional to pop: OK
-## plot(sum(EIF) * pops / sum(pops), inZ, pch = paste0(1:7), main = "in/to", xlab = "model", ylab = "data")
-## abline(a = 0, b = 1, col = 2)
-
-## ## from follows pop with random mixing
-## plot(sum(EIF) * pops / sum(pops), fromZ, pch = paste0(1:7), main = "from", xlab = "model", ylab = "data")
-## abline(a = 0, b = 1, col = 2)
-
-
-## ## random-mixing expectation: proportional to tot TB: lo for 4+5
-## ## NOTE pbm even when prevalence is uniform
-## plot(sum(EIF) * pops * prev / sum(pops * prev), fromZ,
-##   pch = paste0(1:7),
-##   main = "from", xlab = "model", ylab = "data"
-## )
-## abline(a = 0, b = 1, col = 2)
-
-## ## ## OK BUG: chk missing
-## ## plot(chk / sum(chk), fromZ / sum(fromZ))
-## ## abline(a = 0, b = 1)
-
-
-## ## NOTE CHECK DONE:
-## ## cst MM, cst prev:     I/F/
-## ## random mix, cst prev: I/F/
-## ## random mix, var prev: I/FX - works for chk vs from, ie FOI vs from
-## ## => issue in FOI computation
-
-
-## ## test[idx0, p, 12]
-## ## test[idx0, p, 2]
-
-## ## where are infections happening in/to?
-## plot(colSums(FM) * sum(EIF), inZ, pch = paste0(1:7), main = "in/to", xlab = "model", ylab = "data")
-## abline(a = 0, b = 1, col = 2)
-
-## ## where are infections happening from?
-## plot(rowSums(FM) * sum(EIF), fromZ, pch = paste0(1:7), main = "from", xlab = "model", ylab = "data")
-## abline(a = 0, b = 1, col = 2)
-
-## ## comparison
-## CF <- data.table(genomic.flux = c(FM / max(FM)), output.FOI.flux = c(nEIF))
-## CF[, variable := gsub("cum_inf_flux", "", nmz)]
-## CF[, fromid := gsub("\\[", "", gsub(",.\\]", "", variable))]
-
-## ## plot:
-## ggplot(
-##   CF,
-##   aes(
-##     x = genomic.flux, y = output.FOI.flux,
-##     label = variable, shape = fromid, col = fromid
-##   )
-## ) +
-##   geom_abline(intercept = 0, slope = 1, col = 2) +
-##   ## scale_shape(solid=FALSE)+
-##   scale_shape_manual(values = 0:6) +
-##   geom_point(size = 2) +
-##   geom_text_repel(show.legend = FALSE) +
-##   theme_classic() +
-##   ggpubr::grids()
-
-## ## ggsave(here("plots/flux_cfFOI_new.png"), w = 7, h = 6)
-
-
+## data vs note
+ggplot(
+  CF,
+  aes(
+    x = genomic.flux, y = output.note.flux,
+    label = variable, shape = fromid, col = fromid
+  )
+) +
+  geom_abline(intercept = 0, slope = 1, col = 2) +
+  scale_shape_manual(values = 0:6) +
+  geom_point(size = 2) +
+  geom_text_repel(show.legend = FALSE) +
+  theme_classic() +
+  ggpubr::grids()
